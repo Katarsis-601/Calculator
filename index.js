04109a6802502fa02dca0293052a838ed34ee709
Zero <andrianow817@gmail.com>
2024-04-03 19:51:15 +0700

    first commit

--- index.js
+++ index.js
@@ -10,14 +10,23 @@ const arr_btn = () => {
   }
   return arr;
 };
+const getAttribute = (value) => {
+  return value.target.attributes[1].value;
+};
+
 const btn_event = () => {
   arr_btn().map((data) => {
     data.addEventListener("click", (value) => {
-      input_text += value.target.attributes[1].value;
+      if (getAttribute(value) === "AC") {
+        input_text = "";
+      } else if ((getAttribute(value) === "=") & (input_text.length != 0)) {
+        input_text = Function(`return ${input_text}`)();
+      } else {
+        input_text += getAttribute(value);
+      }
       input_field.innerHTML = input_text;
     });
   });
 };
-console.log(input_field);
 
 btn_event();
