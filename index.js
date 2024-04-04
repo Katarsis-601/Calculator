81bc622e37cc49968d947a63eec8f6598d2bc79c
Zero <andrianow817@gmail.com>
2024-04-04 08:48:29 +0700

    propably finish

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const button = document.querySelectorAll("button");
 const input_field = document.getElementsByClassName("calculator_input")[0];
 let input_text = "";
+
+//A Function that return array of all button element
 const arr_btn = () => {
   let i = 0;
   let arr = [];
@@ -10,14 +12,20 @@ const arr_btn = () => {
   }
   return arr;
 };
+
+//this function will get the button metadata attribute
 const getAttribute = (value) => {
   return value.target.attributes[1].value;
 };
+
+//this fuction will add event to all button
 
 const btn_event = () => {
   arr_btn().map((data) => {
     data.addEventListener("click", (value) => {
       if (getAttribute(value) === "AC") {
+        //this condition its like erase input
+        //so whenever button AC clicked it will erase
         input_text = "";
       } else if ((getAttribute(value) === "=") & (input_text.length != 0)) {
         input_text = Function(`return ${input_text}`)();
